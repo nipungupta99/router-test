d4ab604c37176e609877d489d1cf1c463ba4dda4
Nipun <gupta.nipun99@gmail.com>
2022-01-15 17:18:30 +0530

    added more chantges

---
+++ build/precache-manifest.89d33315c1d920d18927314e4c34143f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0134bbeb9be2f9d0d377d88416417adc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5309e351a1d5d6951b73",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b07047f7e81429b855de",
+    "url": "./static/js/2.b92f16dd.chunk.js"
+  },
+  {
+    "revision": "b69dfa28a9772076cbe7",
+    "url": "./static/js/3.34dad17e.chunk.js"
+  },
+  {
+    "revision": "5309e351a1d5d6951b73",
+    "url": "./static/js/main.2e49ad92.chunk.js"
+  },
+  {
+    "revision": "e3e99e9d176ae04f64b0",
+    "url": "./static/js/runtime~main.160038c9.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
